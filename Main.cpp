7390191157de5f66a2ddd67ccc014dd000cc6144
Taeuk Ahn <tu1014@naver.com>
2023-02-14 17:23:55 +0900

    프로젝트 생성

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+
+#include "DataStructure.h"
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello World!\n";
+	printCMake();
+
+
+	return 0;
+}
---
+++ Src/DataStructure.cpp
@@ -0,0 +1,12 @@
+﻿// DataStructure.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include "DataStructure.h"
+
+using namespace std;
+
+int printCMake()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
